c83741ff2b63951ef7a005148cc874b1515e2589
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-05-18 16:18:58 +0100

    adding access token with (dummy) repo, service and handler

---
+++ src/app/application.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	"github.com/alessandroarosio/bookstore_oauth-api/src/domain/access_token"
+	"github.com/alessandroarosio/bookstore_oauth-api/src/http"
+	"github.com/alessandroarosio/bookstore_oauth-api/src/repository/db"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	router = gin.Default()
+)
+
+func StartApplication() {
+	repo := db.NewRepository()
+	atService := access_token.NewService(repo)
+	atHandler := http.NewHandler(atService)
+
+	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
+
+	router.Run(":8080")
+}
---
+++ src/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/alessandroarosio/bookstore_oauth-api/src/app"
+
+func main() {
+	app.StartApplication()
+}
